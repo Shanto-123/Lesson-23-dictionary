4fbe09a8b1982b3964e079e9f315364739fa2e2e
shanto das <114013679+Shanto-123@users.noreply.github.com>
2024-09-30 01:09:24 +0600

    Add files via upload

---
+++ Lesson-23-dictionary.py
@@ -0,0 +1,10 @@
+user = {
+    "Name": "Shanto Chandra Das",
+    "Age" : 23,
+    "Email" : "dshanto2468@gmail.com",
+    "Is_verified":True
+}
+user["Job"]="Teaching"
+print(user)
+##print(user["Name"])
+print(user.get("Is_verified"))
